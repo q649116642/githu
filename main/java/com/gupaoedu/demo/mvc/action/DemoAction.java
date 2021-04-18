195e83b66b8bac854da7b379636c6ad3ada7e295
wangwei <649116642@qq.com>
2021-04-18 20:52:45 +0800

    ss

--- main/java/com/gupaoedu/demo/mvc/action/DemoAction.java
+++ main/java/com/gupaoedu/demo/mvc/action/DemoAction.java
@@ -19,7 +19,7 @@ public class DemoAction {
 
   	@GPAutowired private IDemoService demoService;
 
-	@GPRequestMapping("/query")
+	@GPRequestMapping("/query") //asdsd
 	public void query(HttpServletRequest req, HttpServletResponse resp,
 					  @GPRequestParam("name") String name){
 		String result = demoService.get(name);
